c0cf850c2c72a56f4591e1cc9630488d9577ff82
Jordan Gilliam <gilliamja.te@gmail.com>
2020-03-14 21:14:31 -0400

    feat: added pika/pkg for publishing

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -1,5 +1,2 @@
 export { default as useCookie } from './useCookie';
 export * from './useCookie';
-
-export { default as useIntersectionObserver } from './useIntersectionObserver';
-export * from './useIntersectionObserver';
